fc6b47a479c497b9a19351470ad507b4ad289ae6
SoooBeeeN <ason0819@gmail.com>
2023-11-12 18:18:57 +0900

    html, css

---
+++ KoreaSeries/script.js
@@ -0,0 +1,7 @@
+// 결과예측하기 버튼에 클릭 이벤트 리스너를 추가
+// HTML문서에서 id가 'predict-button'요소를 선택
+document.getElementById('predict-button').addEventListener('click', function() {
+    //results라는 객체
+    // : LG와 KT 각각의 승리 횟수를 추적
+    let results = {"LG"}
+});
